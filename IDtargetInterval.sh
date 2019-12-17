18d9c42c337fce3f26793163c0bc7d61d01efff6
Octocoot <heyudou@gmail.com>
2019-12-17 15:39:28 -0800

    Update IDtargetInterval.sh

--- IDtargetInterval.sh
+++ IDtargetInterval.sh
@@ -27,28 +27,28 @@ RTC_n="gatk3 -T RealignerTargetCreator -R $ref -I ${sample}_normal_mkdp.bam --kn
 IR_t="gatk3 -T IndelRealigner -R $ref -known ${knownIndels} -targetIntervals \$(pwd -P)/${sample}_tumor_realign_target.intervals --noOriginalAlignmentTags -I ${sample}_tumor_mkdp.bam -o ${sample}_tumor_idra.bam"
 IR_n="gatk3 -T IndelRealigner -R $ref -known ${knownIndels} -targetIntervals \$(pwd -P)/${sample}_normal_realign_target.intervals --noOriginalAlignmentTags -I ${sample}_normal_mkdp.bam -o ${sample}_normal_idra.bam"
 
-printf "$header">jobs/IDtargetInterval/${sample}_TIDtargetInterval_2.pbs
-printf "${Tjobname}">>jobs/IDtargetInterval/${sample}_TIDtargetInterval_2.pbs
-printf "$header">jobs/IDtargetInterval/${sample}_NIDtargetInterval_2.pbs
-printf "${Njobname}">>jobs/IDtargetInterval/${sample}_NIDtargetInterval_2.pbs
-echo source ~/.bashrc>>jobs/IDtargetInterval/${sample}_TIDtargetInterval_2.pbs
-echo source activate evc_py3>>jobs/IDtargetInterval/${sample}_TIDtargetInterval_2.pbs
-echo source ~/.bashrc>>jobs/IDtargetInterval/${sample}_NIDtargetInterval_2.pbs
-echo source activate evc_py3>>jobs/IDtargetInterval/${sample}_NIDtargetInterval_2.pbs
+printf "$header">jobs/refine/${sample}_TIDtargetInterval_2.pbs
+printf "${Tjobname}">>jobs/refine/${sample}_TIDtargetInterval_2.pbs
+printf "$header">jobs/refine/${sample}_NIDtargetInterval_2.pbs
+printf "${Njobname}">>jobs/refine/${sample}_NIDtargetInterval_2.pbs
+echo source ~/.bashrc>>jobs/refine/${sample}_TIDtargetInterval_2.pbs
+echo source activate evc_py3>>jobs/refine/${sample}_TIDtargetInterval_2.pbs
+echo source ~/.bashrc>>jobs/refine/${sample}_NIDtargetInterval_2.pbs
+echo source activate evc_py3>>jobs/refine/${sample}_NIDtargetInterval_2.pbs
 
-echo cd ${out}/${sample}_talign/>>jobs/IDtargetInterval/${sample}_TIDtargetInterval_2.pbs
-echo cd ${out}/${sample}_nalign/>>jobs/IDtargetInterval/${sample}_NIDtargetInterval_2.pbs
+echo cd ${out}/${sample}_talign/>>jobs/refine/${sample}_TIDtargetInterval_2.pbs
+echo cd ${out}/${sample}_nalign/>>jobs/refine/${sample}_NIDtargetInterval_2.pbs
 
-echo 'echo starting RealignerTargetCreator at $(date)'>>jobs/IDtargetInterval/${sample}_TIDtargetInterval_2.pbs
-echo 'echo starting RealignerTargetCreator at $(date)'>>jobs/IDtargetInterval/${sample}_NIDtargetInterval_2.pbs
-echo 'targetS=$SECONDS'>>jobs/IDtargetInterval/${sample}_TIDtargetInterval_2.pbs
-echo 'targetS=$SECONDS'>>jobs/IDtargetInterval/${sample}_NIDtargetInterval_2.pbs
-echo ${RTC_t}>>jobs/IDtargetInterval/${sample}_TIDtargetInterval_2.pbs
-echo ${RTC_n}>>jobs/IDtargetInterval/${sample}_NIDtargetInterval_2.pbs
-echo 'targetT=$(($SECONDS - $targetS))'>>jobs/IDtargetInterval/${sample}_TIDtargetInterval_2.pbs
-echo 'targetT=$(($SECONDS - $targetS))'>>jobs/IDtargetInterval/${sample}_NIDtargetInterval_2.pbs
-echo 'echo target interval creation took $targetT seconds'>>jobs/IDtargetInterval/${sample}_TIDtargetInterval_2.pbs
-echo 'echo target interval creation took $targetT seconds'>>jobs/IDtargetInterval/${sample}_NIDtargetInterval_2.pbs
+echo 'echo starting RealignerTargetCreator at $(date)'>>jobs/refine/${sample}_TIDtargetInterval_2.pbs
+echo 'echo starting RealignerTargetCreator at $(date)'>>jobs/refine/${sample}_NIDtargetInterval_2.pbs
+echo 'targetS=$SECONDS'>>jobs/refine/${sample}_TIDtargetInterval_2.pbs
+echo 'targetS=$SECONDS'>>jobs/refine/${sample}_NIDtargetInterval_2.pbs
+echo ${RTC_t}>>jobs/refine/${sample}_TIDtargetInterval_2.pbs
+echo ${RTC_n}>>jobs/v/${sample}_NIDtargetInterval_2.pbs
+echo 'targetT=$(($SECONDS - $targetS))'>>jobs/refine/${sample}_TIDtargetInterval_2.pbs
+echo 'targetT=$(($SECONDS - $targetS))'>>jobs/refine/${sample}_NIDtargetInterval_2.pbs
+echo 'echo target interval creation took $targetT seconds'>>jobs/refine/${sample}_TIDtargetInterval_2.pbs
+echo 'echo target interval creation took $targetT seconds'>>jobs/refine/${sample}_NIDtargetInterval_2.pbs
 
-echo 'echo job finished at $(date)'>>jobs/IDtargetInterval/${sample}_TIDtargetInterval_2.pbs
-echo 'echo job finished at $(date)'>>jobs/IDtargetInterval/${sample}_NIDtargetInterval_2.pbs
+echo 'echo job finished at $(date)'>>jobs/refine/${sample}_TIDtargetInterval_2.pbs
+echo 'echo job finished at $(date)'>>jobs/refine/${sample}_NIDtargetInterval_2.pbs
