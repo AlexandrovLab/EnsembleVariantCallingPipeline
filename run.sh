c46d9ceee0453f369290bc3a3813cb16c279289b
Octocoot <heyudou@gmail.com>
2019-12-17 13:11:28 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 email=phoebehe@eng.ucsd.edu
 ref=/restricted/alexandrov-group/shared/Reference_Genomes/GRCh38.d1.vd1/GRCh38.d1.vd1.fa
-path=/restricted/alexandrov-group/shared/precancer_analysis/tissue_types/oral/olivier_oral/paired_end
-out=$1
+path=$1
+out=$2
 knownIndels=/restricted/alexandrov-group/shared/Reference_Genomes/known_indels/resources_broad_hg38_v0_Homo_sapiens_assembly38.known_indels.vcf
+dbSNP=
 USAGE="\nMissing input arguments..\n
 USAGE:\trun.sh \\
 	path/to/project\n\n"
-if [ "$1" == "" ] 
+if [ "$1" == "" ] || [ "$2" == "" ] 
 then printf "$USAGE"
 else
 cd $out
@@ -18,6 +19,6 @@ sample=$(echo $line|cut -d ' ' -f1)
 tumor=$(echo $line|cut -d ' ' -f2)
 normal=$(echo $line|cut -d ' ' -f3)
 ./align_template.sh $email $sample $tumor $normal $ref $path $out
-./coclean_template.sh $email $sample $ref $knownIndels $out; done
+./coclean_template.sh $email $sample $ref $knownIndels $out $dbSNP; done
 #for f in jobs/*align*.pbs;do qsub $f;done
 fi
