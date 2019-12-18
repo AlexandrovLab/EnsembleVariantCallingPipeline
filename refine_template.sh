c9cabaf33033a2574fa83f0a62f0790184449533
Octocoot <heyudou@gmail.com>
2019-12-17 17:16:52 -0800

    Update refine_template.sh

--- refine_template.sh
+++ refine_template.sh
@@ -23,11 +23,8 @@ Njobname="#PBS -N EVC_Nrefine_${sample}
 #PBS -o ${sample}_Nrefine_2.o
 "
 
-RTC_t="gatk3 -T RealignerTargetCreator -R $ref -I ${sample}_tumor_mkdp.bam --known ${knownIndels} -o ${sample}_tumor_realign_target.intervals -Xmx\$(free -h| grep Mem | awk '{print \$4}')"
-RTC_n="gatk3 -T RealignerTargetCreator -R $ref -I ${sample}_normal_mkdp.bam --known ${knownIndels} -o ${sample}_normal_realign_target.intervals -Xmx\$(free -h| grep Mem | awk '{print \$4}')"
-
-IR_t="gatk3 -T IndelRealigner -R $ref -known ${knownIndels} -targetIntervals \$(pwd -P)/${sample}_tumor_realign_target.intervals --noOriginalAlignmentTags -I ${sample}_tumor_mkdp.bam -o ${sample}_tumor_idra.bam -Xmx\$(free -h| grep Mem | awk '{print \$4}')"
-IR_n="gatk3 -T IndelRealigner -R $ref -known ${knownIndels} -targetIntervals \$(pwd -P)/${sample}_normal_realign_target.intervals --noOriginalAlignmentTags -I ${sample}_normal_mkdp.bam -o ${sample}_normal_idra.bam -Xmx\$(free -h| grep Mem | awk '{print \$4}')"
+IR_t="gatk3 -T IndelRealigner -R $ref -known ${knownIndels} -targetIntervals \$(pwd -P)/${sample}_union_realign_target.intervals --noOriginalAlignmentTags -I ${sample}_tumor_mkdp.bam -o ${sample}_tumor_idra.bam -Xmx\$(free -h| grep Mem | awk '{print \$4}')"
+IR_n="gatk3 -T IndelRealigner -R $ref -known ${knownIndels} -targetIntervals \$(pwd -P)/${sample}_union_realign_target.intervals --noOriginalAlignmentTags -I ${sample}_normal_mkdp.bam -o ${sample}_normal_idra.bam -Xmx\$(free -h| grep Mem | awk '{print \$4}')"
 
 BR_t="gatk3 -T BaseRecalibrator -R $ref -I ${sample}_tumor_idra.bam --knownSites $dbsnp -o ${sample}_tumor_bqsr.grp -Xmx\$(free -h| grep Mem | awk '{print $4}')"
 BR_n="gatk3 -T BaseRecalibrator -R $ref -I ${sample}_normal_idra.bam --knownSites $dbsnp -o ${sample}_normal_bqsr.grp -Xmx\$(free -h| grep Mem | awk '{print $4}')"
@@ -45,19 +42,6 @@ echo source ~/.bashrc>>jobs/refine/${sample}_Nrefine_2.pbs
 echo source activate evc_py3>>jobs/refine/${sample}_Nrefine_2.pbs
 echo cd ${out}/${sample}/>>jobs/refine/${sample}_Trefine_2.pbs
 echo cd ${out}/${sample}/>>jobs/refine/${sample}_Nrefine_2.pbs
-
-
-
-echo 'echo starting RealignerTargetCreator at $(date)'>>jobs/refine/${sample}_Trefine_2.pbs
-echo 'echo starting RealignerTargetCreator at $(date)'>>jobs/refine/${sample}_Nrefine_2.pbs
-echo 'targetS=$SECONDS'>>jobs/refine/${sample}_Trefine_2.pbs
-echo 'targetS=$SECONDS'>>jobs/refine/${sample}_Nrefine_2.pbs
-echo ${RTC_t}>>jobs/refine/${sample}_Trefine_2.pbs
-echo ${RTC_n}>>jobs/refine/${sample}_Nrefine_2.pbs
-echo 'targetT=$(($SECONDS - $targetS))'>>jobs/refine/${sample}_Trefine_2.pbs
-echo 'targetT=$(($SECONDS - $targetS))'>>jobs/refine/${sample}_Nrefine_2.pbs
-echo 'echo target interval creation took $targetT seconds'>>jobs/refine/${sample}_Trefine_2.pbs
-echo 'echo target interval creation took $targetT seconds'>>jobs/refine/${sample}_Nrefine_2.pbs
 
 
 echo 'echo starting IndelRealigner at $(date)'>>jobs/refine/${sample}_Trefine_2.pbs
