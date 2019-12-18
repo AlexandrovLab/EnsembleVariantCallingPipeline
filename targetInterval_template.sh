741b1bb22da6c1e99ae7d181110cfc382f86279a
Octocoot <heyudou@gmail.com>
2019-12-17 17:20:12 -0800

    Update targetInterval_template.sh

--- targetInterval_template.sh
+++ targetInterval_template.sh
@@ -20,7 +20,7 @@ header="#!/bin/bash
 
 RTC_t="gatk3 -T RealignerTargetCreator -R $ref -I ${sample}_tumor_mkdp.bam --known ${knownIndels} -o ${sample}_tumor_realign_target.intervals -Xmx\$(free -h| grep Mem | awk '{print \$4}')"
 RTC_n="gatk3 -T RealignerTargetCreator -R $ref -I ${sample}_normal_mkdp.bam --known ${knownIndels} -o ${sample}_normal_realign_target.intervals -Xmx\$(free -h| grep Mem | awk '{print \$4}')"
-
+RTC_union="picard IntervalListTools ACTION=UNION SORT=true UNIQUE=true I=${sample}_tumor_realign_target.intervals I=${sample}_normal_realign_target.intervals O=${sample}_union_realign_target.intervals "
 printf "$header">jobs/refine/${sample}_targetInterval.pbs
 
 echo source ~/.bashrc>>jobs/refine/${sample}_targetInterval.pbs
@@ -37,3 +37,7 @@ echo ${RTC_n}>>jobs/refine/${sample}_targetInterval.pbs
 echo 'targetN=$(($SECONDS - $targetS))'>>jobs/refine/${sample}_targetInterval.pbs
 echo 'echo target interval creation took $targetT seconds for tumor'>>jobs/refine/${sample}_targetInterval.pbs
 echo 'echo target interval creation took $targetN seconds for normal'>>jobs/refine/${sample}_targetInterval.pbs
+
+echo source ~/.bashrc>>jobs/refine/${sample}_targetInterval.pbs
+echo source activate cvc_py3>>jobs/refine/${sample}_targetInterval.pbs
+echo 'echo target interval creation took $targetT seconds for tumor'>>jobs/refine/${sample}_targetInterval.pbs
