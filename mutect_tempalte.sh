24e3629dfb830be1dc7be738fdb14d29fe55885d
Octocoot <heyudou@gmail.com>
2019-12-19 23:23:09 -0800

    Update mutect_tempalte.sh

--- mutect_tempalte.sh
+++ mutect_tempalte.sh
@@ -3,7 +3,7 @@ email=$1
 sample=$2
 ref=$3
 out=$4
-#type=$5
+pon=$5
 normal=${out}/${sample}/${sample}_normal_final.bam
 tumor=${out}/${sample}/${sample}_tumor_final.bam
 template="#!/bin/bash
@@ -23,12 +23,12 @@ mkdir -p ${out}/${sample}/mutect
 cd ${out}/${sample}/mutect
 "
 
-mutect_cmd="gatk Mutect2 -R $ref -I tumor.bam -germline-resource $dbSNP -pon panel_of_normals.vcf --f1r2-tar-gz ${sample}_f1r2.tar.gz -O ${sample}_unfiltered.vcf"
+mutect_cmd="gatk Mutect2 -R $ref -I ${sample}_tumor_final.bam -germline-resource $dbSNP -pon $pon --f1r2-tar-gz ${sample}_f1r2.tar.gz -O ${sample}_unfiltered.vcf"
    
 mutect_orientation_2="gatk LearnReadOrientationModel -I ${sample}_f1r2.tar.gz -O ${sample}_read-orientation-model.tar.gz"
 
-mutect_contamin_1="gatk GetPileupSummaries -I tumor.bam -V chr17_small_exac_common_3_grch38.vcf.gz -L chr17_small_exac_common_3_grch38.vcf.gz -O getpileupsummaries.table"
+mutect_contamin_1="gatk GetPileupSummaries -I ${sample}_tumor_final.bam -V chr17_small_exac_common_3_grch38.vcf.gz -L chr17_small_exac_common_3_grch38.vcf.gz -O ${sample}_getpileupsummaries.table"
 
-mutect_contamin_2="gatk CalculateContamination -I getpileupsummaries.table -tumor-segmentation segments.table -O calculatecontamination.table"
+mutect_contamin_2="gatk CalculateContamination -I ${sample}_getpileupsummaries.table -tumor-segmentation ${sample}_segments.table -O ${sample}_calculatecontamination.table"
 
-mutect_filter="gatk FilterMutectCalls -V ${sample}_unfiltered.vcf [--tumor-segmentation segments.table] --contamination-table contamination.table] --ob-priors ${sample}_read-orientation-model.tar.gz -O ${sample}_filtered.vcf"
+mutect_filter="gatk FilterMutectCalls -V ${sample}_unfiltered.vcf --tumor-segmentation ${sample}_segments.table --contamination-table ${sample}_contamination.table --ob-priors ${sample}_read-orientation-model.tar.gz -O ${sample}_filtered.vcf"
