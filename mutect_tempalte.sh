31fba03e53039e58c35507dcba3e2c050190969c
Octocoot <heyudou@gmail.com>
2019-12-19 16:28:54 -0800

    Update mutect_tempalte.sh

--- mutect_tempalte.sh
+++ mutect_tempalte.sh
@@ -17,23 +17,3 @@ header="#!/bin/bash
 #PBS -e ${sample}_mutect.e
 #PBS -o ${sample}_mutect.o
 "
-
-strelka_config_exome="configureStrelkaSomaticWorkflow.py --exome --referenceFasta $ref --normalBam $normal --tumorBam $tumor --runDir ${out}/${sample}/strelka"
-strelka_config_genome="configureStrelkaSomaticWorkflow.py --referenceFasta $ref --normalBam $normal --tumorBam $tumor --runDir ${out}/${sample}/strelka"
-#runstrelka="python2 %s -m local -j $(nproc)"
-
-printf "$header">jobs/strelka/${sample}_strelka.pbs
-echo source ~/.bashrc>>jobs/strelka/${sample}_strelka.pbs
-echo source activate cvc_py2>>jobs/strelka/${sample}_strelka.pbs
-echo mkdir -p ${out}/${sample}/strelka>>jobs/strelka/${sample}_strelka.pbs
-echo cd ${out}/${sample}/strelka>>jobs/strelka/${sample}_strelka.pbs
-
-echo 'echo starting strelka at $(date)'>>jobs/strelka/${sample}_strelka.pbs
-echo 'strelkaConfigS=$SECONDS'>>jobs/strelka/${sample}_strelka.pbs
-if [ type = "exome" ]
-then echo ${strelka_config_exome}>>jobs/strelka/${sample}_strelka.pbs
-else echo ${strelka_config_genome}>>jobs/strelka/${sample}_strelka.pbs
-fi
-#echo ${runstrelka}>>jobs/strelka/${sample}_strelka.pbs
-echo 'strelkaConfigT=$(($SECONDS - $strelkaConfigS))'>>jobs/strelka/${sample}_strelka.pbs
-echo 'echo strelka Configuration took $strelkaConfigT seconds'>>jobs/strelka/${sample}_strelka.pbs
