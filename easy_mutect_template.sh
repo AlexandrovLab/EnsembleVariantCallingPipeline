6fa90065db4587ec2d5bdca398b686dc0f60e63d
Octocoot <heyudou@gmail.com>
2019-12-20 18:38:43 -0800

    Update easy_mutect_template.sh

--- easy_mutect_template.sh
+++ easy_mutect_template.sh
@@ -29,14 +29,15 @@ cd ${out}/${sample}/mutectEASY
 printf "$template">jobs/mutectEASY/${sample}_mutectEASY.pbs
 
 mutect_cmd="gatk Mutect2 --native-pair-hmm-threads $(nproc) --germline-resource $dbsnp --af-of-alleles-not-in-resource 0.00003125 --reference $ref --panel-of-normals $pon --input $normal --tumor-sample ${sample}_tumor --input $tumor --normal-sample ${sample}_normal --output ${sample}_mutect_unfiltered.vcf"
-filter_cmd="filter_mutect_cmd = 'gatk FilterMutectCalls -V ${sample}_mutect_unfiltered.vcf -O ${sample}_mutect_filtered.vcf'"
+filter_cmd="gatk FilterMutectCalls -V ${sample}_mutect_unfiltered.vcf -O ${sample}_mutect_filtered.vcf'"
 
-
+: <<'END'
 echo 'echo starting mutect command at $(date)....'>>jobs/mutectEASY/${sample}_mutectEASY.pbs
 echo 'mutectS=$SECONDS'>>jobs/mutectEASY/${sample}_mutectEASY.pbs
 echo ${mutect_cmd}>>jobs/mutectEASY/${sample}_mutectEASY.pbs
 echo 'mutectT=$(($SECONDS-$mutectS))'>>jobs/mutectEASY/${sample}_mutectEASY.pbs
 echo 'echo mutect command took $mutectT seconds'>>jobs/mutectEASY/${sample}_mutectEASY.pbs
+END
 
 echo 'echo starting mutect filter at $(date)....'>>jobs/mutectEASY/${sample}_mutectEASY.pbs
 echo 'MfilterS=$SECONDS'>>jobs/mutectEASY/${sample}_mutectEASY.pbs
