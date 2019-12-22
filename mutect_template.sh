05cb60738f3a4c24f4b4063b49e454b31998dbcd
Octocoot <heyudou@gmail.com>
2019-12-21 23:07:46 -0800

    Update mutect_template.sh

--- mutect_template.sh
+++ mutect_template.sh
@@ -5,6 +5,7 @@ ref=$3
 out=$4
 pon=$5
 type=$6
+dbsnp=$7
 
 # decide --af-of-alleles-not-in-resource based on exome or genome data type
 if [ $type == "exome" ]
@@ -36,9 +37,9 @@ mutect_cmd="gatk Mutect2 -R $ref -pon $pon -germline-resource $dbSNP --native-pa
   
 mutect_orientation="gatk LearnReadOrientationModel -I ${sample}_f1r2.tar.gz -O ${sample}_read-orientation-model.tar.gz"
 
-mutect_contamin1="gatk GetPileupSummaries -I ${sample}_tumor_final.bam -V chr17_small_exac_common_3_grch38.vcf.gz -L chr17_small_exac_common_3_grch38.vcf.gz -O ${sample}_getpileupsummaries.table"
+mutect_pileupsum="gatk GetPileupSummaries -I ${sample}_tumor_final.bam -V $dbsnp -O ${sample}_getpileupsummaries.table"
 
-mutect_contamin2="gatk CalculateContamination -I ${sample}_getpileupsummaries.table -tumor-segmentation ${sample}_segments.table -O ${sample}_calculatecontamination.table"
+mutect_contamin="gatk CalculateContamination -I ${sample}_getpileupsummaries.table -tumor-segmentation ${sample}_segments.table -O ${sample}_calculatecontamination.table"
 
 mutect_filter="gatk FilterMutectCalls -V ${sample}_unfiltered.vcf --tumor-segmentation ${sample}_segments.table --contamination-table ${sample}_contamination.table --ob-priors ${sample}_read-orientation-model.tar.gz -O ${sample}_filtered.vcf"
 
@@ -51,21 +52,21 @@ echo ${mutect_cmd}>>jobs/mutect/${sample}_mutect.pbs
 echo 'mutectT=$(($SECONDS-$mutectS))/3600'>>jobs/mutect/${sample}_mutect.pbs
 echo 'echo mutect command took $mutectT hours'>>jobs/mutect/${sample}_mutect.pbs
 
-echo 'echo starting mutect Read Orientation at $(date)....'>>jobs/mutect/${sample}_mutect.pbs
+echo 'echo starting Read Orientation at $(date)....'>>jobs/mutect/${sample}_mutect.pbs
 echo 'readorientationtS=$SECONDS'>>jobs/mutect/${sample}_mutect.pbs
 echo ${mutect_orientation}>>jobs/mutect/${sample}_mutect.pbs
 echo 'readorientationtT=$(($SECONDS-$readorientationtS))'>>jobs/mutect/${sample}_mutect.pbs
 echo 'echo mutect Read Orientation took $readorientationtT seconds'>>jobs/mutect/${sample}_mutect.pbs
 
-echo 'echo starting mutect contamination part1 at $(date)....'>>jobs/mutect/${sample}_mutect.pbs
+echo 'echo starting GetPileupSummaries at $(date)....'>>jobs/mutect/${sample}_mutect.pbs
 echo 'ct1S=$SECONDS'>>jobs/mutect/${sample}_mutect.pbs
-echo ${mutect_contamin1}>>jobs/mutect/${sample}_mutect.pbs
+echo ${mutect_pileupsum}>>jobs/mutect/${sample}_mutect.pbs
 echo 'ct1T=$(($SECONDS-$ct1S))'>>jobs/mutect/${sample}_mutect.pbs
 echo 'echo mutect contamination part1 took $ct1T seconds'>>jobs/mutect/${sample}_mutect.pbs
 
-echo 'echo starting mutect contamination part2 at $(date)....'>>jobs/mutect/${sample}_mutect.pbs
+echo 'echo starting calculating contaminatio at $(date)....'>>jobs/mutect/${sample}_mutect.pbs
 echo 'ct2S=$SECONDS'>>jobs/mutect/${sample}_mutect.pbs
-echo ${mutect_contamin2}>>jobs/mutect/${sample}_mutect.pbs
+echo ${mutect_contamin}>>jobs/mutect/${sample}_mutect.pbs
 echo 'ct2T=$(($SECONDS-$ct2S))'>>jobs/mutect/${sample}_mutect.pbs
 echo 'echo mutect contamination part1 took $ct2T seconds'>>jobs/mutect/${sample}_mutect.pbs
 
