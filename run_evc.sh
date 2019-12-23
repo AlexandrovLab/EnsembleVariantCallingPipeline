b3c7b0dbc4838163145fdc2fd4a0e245254267d4
Octocoot <heyudou@gmail.com>
2019-12-22 22:14:52 -0800

    Update run_evc.sh

--- run_evc.sh
+++ run_evc.sh
@@ -30,7 +30,7 @@ mkdir -p ${out}/jobs/mutect
 mkdir -p ${out}/jobs/check_and_go
 
 cd $out/jobs/check_and_go
-printf "cd ${out}/jobs/align\nfor f in *pbs;do qsub \$f;done|awk -F\".\" '{print /$1}'>>${out}/jobs/check/align_job_IDs.txt\n">start_align.sh
+printf "cd ${out}/jobs/align\nfor f in *pbs;do qsub \$f;done|awk -F\".\" '{print \$1}'>>${out}/jobs/check/align_job_IDs.txt\n">start_align.sh
 chmod +x start_align.sh
 
 cd $out
