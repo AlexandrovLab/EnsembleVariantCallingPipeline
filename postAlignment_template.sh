fc08da39d6431489292df725334e26a71d85216c
Octocoot <heyudou@gmail.com>
2020-01-15 10:18:27 -0800

    Update postAlignment_template.sh

--- postAlignment_template.sh
+++ postAlignment_template.sh
@@ -20,11 +20,11 @@ header="#!/bin/bash
 #PBS -m bea
 #PBS -M ${email}
 #PBS -V
-#PBS -N EVC_targetInterval_${sample}
-#PBS -e ${sample}_targetInterval.e
-#PBS -o ${sample}_targetInterval.o
+#PBS -N EVC_postAlign_${sample}
+#PBS -e ${sample}_postAlign.e
+#PBS -o ${sample}_postAlign.o
 "
-printf "$header">jobs/refine/${sample}_targetInterval.pbs
+printf "$header">jobs/postAlign/${sample}_postAlign.pbs
 
 
 #### Define Commands #####
@@ -62,59 +62,59 @@ PR_n="gatk3 -T PrintReads -R $ref -I ${sample}_normal_idra.bam --BQSR ${sample}_
 pon_cmd="gatk Mutect2 --independent-mates -R $ref --native-pair-hmm-threads \$(nproc) -I ${sample}_normal_final.bam --max-mnp-distance 0 -O ${sample}_PON.vcf.gz"
 ############################
 
-echo source ~/.bashrc>>jobs/refine/${sample}_targetInterval.pbs
-echo source activate evc_gatk3>>jobs/refine/${sample}_targetInterval.pbs
-echo cd ${out}/${sample}/>>jobs/refine/${sample}_targetInterval.pbs
+echo source ~/.bashrc>>jobs/postAlign/${sample}_postAlign.pbs
+echo source activate evc_gatk3>>jobs/postAlign/${sample}_postAlign.pbs
+echo cd ${out}/${sample}/>>jobs/postAlign/${sample}_postAlign.pbs
 
 ## Create target interval ##
-echo '########## Target Interval #########'>>jobs/refine/${sample}_targetInterval.pbs
-echo 'echo starting RealignerTargetCreator at $(date)'>>jobs/refine/${sample}_targetInterval.pbs
-echo 'targetS=$SECONDS'>>jobs/refine/${sample}_targetInterval.pbs
-echo $RTC>>jobs/refine/${sample}_targetInterval.pbs
-echo 'targetT=$(($SECONDS - $targetS))'>>jobs/refine/${sample}_targetInterval.pbs
-echo "echo target interval creation took \$(echo a|awk '{print '\"\$targetT\"'/3600}') hours">>jobs/refine/${sample}_targetInterval.pbs
-echo 'echo Target Interval Created at $(date)'>>jobs/refine/${sample}_Nrefine.pbs
+echo '########## Target Interval #########'>>jobs/postAlign/${sample}_postAlign.pbs
+echo 'echo starting RealignerTargetCreator at $(date)'>>jobs/postAlign/${sample}_postAlign.pbs
+echo 'targetS=$SECONDS'>>jobs/postAlign/${sample}_postAlign.pbs
+echo $RTC>>jobs/postAlign/${sample}_postAlign.pbs
+echo 'targetT=$(($SECONDS - $targetS))'>>jobs/postAlign/${sample}_postAlign.pbs
+echo "echo target interval creation took \$(echo a|awk '{print '\"\$targetT\"'/3600}') hours">>jobs/postAlign/${sample}_postAlign.pbs
+echo 'echo Target Interval Created at $(date)'>>jobs/postAlign/${sample}_postAlign.pbs
 
 ## Refine Alignemnt for Normal ##
-echo '########## Normal Refine #########'>>jobs/refine/${sample}_targetInterval.pbs
-echo 'echo starting IndelRealigner at $(date)'>>jobs/refine/${sample}_Nrefine.pbs
-echo 'idraS=$SECONDS'>>jobs/refine/${sample}_Nrefine.pbs
-echo ${IR_n}>>jobs/refine/${sample}_Nrefine.pbs
-echo 'idraT=$(($SECONDS - $idraS))'>>jobs/refine/${sample}_Nrefine.pbs
-echo "echo Indel Realignemnt took \$(echo a|awk '{print '\"\$idraT\"'/3600}') hours">>jobs/refine/${sample}_Nrefine.pbs
-echo 'echo starting BaseRecalibrator at $(date)'>>jobs/refine/${sample}_Nrefine.pbs
-echo 'barcS=$SECONDS'>>jobs/refine/${sample}_Nrefine.pbs
-echo ${BR_n}>>jobs/refine/${sample}_Nrefine.pbs
-echo 'barcT=$(($SECONDS - $barcS))'>>jobs/refine/${sample}_Nrefine.pbs
-echo "echo Base Recalibration took \$(echo a|awk '{print '\"\$barcT\"'/3600}') hours">>jobs/refine/${sample}_Nrefine.pbs
-echo 'echo starting PrintReads at $(date)'>>jobs/refine/${sample}_Nrefine.pbs
-echo 'prS=$SECONDS'>>jobs/refine/${sample}_Nrefine.pbs
-echo ${PR_n}>>jobs/refine/${sample}_Nrefine.pbs
-echo 'prT=$(($SECONDS - $prS))'>>jobs/refine/${sample}_Nrefine.pbs
-echo "echo Printing Reads took \$(echo a|awk '{print '\"\$prT\"'/3600}') hours">>jobs/refine/${sample}_Nrefine.pbs
-echo 'echo Normal refinement finished at $(date)'>>jobs/refine/${sample}_Nrefine.pbs
+echo '########## Normal Refine #########'>>jobs/postAlign/${sample}_postAlign.pbs
+echo 'echo starting IndelRealigner at $(date)'>>jobs/postAlign/${sample}_postAlign.pbs
+echo 'idraS=$SECONDS'>>jobs/postAlign/${sample}_postAlign.pbs
+echo ${IR_n}>>jobs/postAlign/${sample}_postAlign.pbs
+echo 'idraT=$(($SECONDS - $idraS))'>>jobs/postAlign/${sample}_postAlign.pbs
+echo "echo Indel Realignemnt took \$(echo a|awk '{print '\"\$idraT\"'/3600}') hours">>jobs/postAlign/${sample}_postAlign.pbs
+echo 'echo starting BaseRecalibrator at $(date)'>>jobs/postAlign/${sample}_postAlign.pbs
+echo 'barcS=$SECONDS'>>jobs/postAlign/${sample}_postAlign.pbs
+echo ${BR_n}>>jobs/postAlign/${sample}_postAlign.pbs
+echo 'barcT=$(($SECONDS - $barcS))'>>jobs/postAlign/${sample}_postAlign.pbs
+echo "echo Base Recalibration took \$(echo a|awk '{print '\"\$barcT\"'/3600}') hours">>jobs/postAlign/${sample}_postAlign.pbs
+echo 'echo starting PrintReads at $(date)'>>jobs/postAlign/${sample}_postAlign.pbs
+echo 'prS=$SECONDS'>>jobs/postAlign/${sample}_postAlign.pbs
+echo ${PR_n}>>jobs/postAlign/${sample}_postAlign.pbs
+echo 'prT=$(($SECONDS - $prS))'>>jobs/postAlign/${sample}_postAlign.pbs
+echo "echo Printing Reads took \$(echo a|awk '{print '\"\$prT\"'/3600}') hours">>jobs/postAlign/${sample}_postAlign.pbs
+echo 'echo Normal refinement finished at $(date)'>>jobs/postAlign/${sample}_postAlign.pbs
 
 ## Refine Alignemnt for Tumor ##
-echo '########## Tumor Refine #########'>>jobs/refine/${sample}_targetInterval.pbs
-echo 'echo starting IndelRealigner at $(date)'>>jobs/refine/${sample}_Trefine.pbs
-echo 'idraS=$SECONDS'>>jobs/refine/${sample}_Trefine.pbs
-echo ${IR_t}>>jobs/refine/${sample}_Trefine.pbs
-echo 'idraT=$(($SECONDS - $idraS))'>>jobs/refine/${sample}_Trefine.pbs
-echo "echo Indel Realignemnt took \$(echo a|awk '{print '\"\$idraT\"'/3600}') hours">>jobs/refine/${sample}_Trefine.pbs
-echo 'echo starting BaseRecalibrator at $(date)'>>jobs/refine/${sample}_Trefine.pbs
-echo 'barcS=$SECONDS'>>jobs/refine/${sample}_Trefine.pbs
-echo ${BR_t}>>jobs/refine/${sample}_Trefine.pbs
-echo 'barcT=$(($SECONDS - $barcS))'>>jobs/refine/${sample}_Trefine.pbs
-echo "echo Base Recalibration took \$(echo a|awk '{print '\"\$barcT\"'/3600}') hours">>jobs/refine/${sample}_Trefine.pbs
-echo 'echo starting PrintReads at $(date)'>>jobs/refine/${sample}_Trefine.pbs
-echo 'prS=$SECONDS'>>jobs/refine/${sample}_Trefine.pbs
-echo ${PR_t}>>jobs/refine/${sample}_Trefine.pbs
-echo 'prT=$(($SECONDS - $prS))'>>jobs/refine/${sample}_Trefine.pbs
-echo "echo Printing Reads took \$(echo a|awk '{print '\"\$prT\"'/3600}') hours">>jobs/refine/${sample}_Trefine.pbs
-echo 'echo Tumor refinement finished at $(date)'>>jobs/refine/${sample}_Trefine.pbs
+echo '########## Tumor Refine #########'>>jobs/postAlign/${sample}_postAlign.pbs
+echo 'echo starting IndelRealigner at $(date)'>>jobs/postAlign/${sample}_postAlign.pbs
+echo 'idraS=$SECONDS'>>jobs/postAlign/${sample}_postAlign.pbs
+echo ${IR_t}>>jobs/postAlign/${sample}_postAlign.pbs
+echo 'idraT=$(($SECONDS - $idraS))'>>jobs/postAlign/${sample}_postAlign.pbs
+echo "echo Indel Realignemnt took \$(echo a|awk '{print '\"\$idraT\"'/3600}') hours">>jobs/postAlign/${sample}_postAlign.pbs
+echo 'echo starting BaseRecalibrator at $(date)'>>jobs/postAlign/${sample}_postAlign.pbs
+echo 'barcS=$SECONDS'>>jobs/postAlign/${sample}_postAlign.pbs
+echo ${BR_t}>>jobs/postAlign/${sample}_postAlign.pbs
+echo 'barcT=$(($SECONDS - $barcS))'>>jobs/postAlign/${sample}_postAlign.pbs
+echo "echo Base Recalibration took \$(echo a|awk '{print '\"\$barcT\"'/3600}') hours">>jobs/postAlign/${sample}_postAlign.pbs
+echo 'echo starting PrintReads at $(date)'>>jobs/postAlign/${sample}_postAlign.pbs
+echo 'prS=$SECONDS'>>jobs/postAlign/${sample}_postAlign.pbs
+echo ${PR_t}>>jobs/postAlign/${sample}_postAlign.pbs
+echo 'prT=$(($SECONDS - $prS))'>>jobs/postAlign/${sample}_postAlign.pbs
+echo "echo Printing Reads took \$(echo a|awk '{print '\"\$prT\"'/3600}') hours">>jobs/postAlign/${sample}_postAlign.pbs
+echo 'echo Tumor refinement finished at $(date)'>>jobs/postAlign/${sample}_postAlign.pbs
 
 ## Panel of Normals ##
-echo '########## Panel of Normals #########'>>jobs/refine/${sample}_targetInterval.pbs
-echo 'ponT=$(($SECONDS - $ponS))'>>jobs/pon/${sample}_pon.pbs
-echo "echo generating PON vcf took \$(echo a|awk '{print '\"\$ponT\"'/3600}') hours">>jobs/pon/${sample}_pon.pbs
-echo 'echo PON finished at $(date)'>>jobs/pon/${sample}_pon.pbs
+echo '########## Panel of Normals #########'>>jobs/postAlign/${sample}_postAlign.pbs
+echo 'ponT=$(($SECONDS - $ponS))'>>jobs/postAlign/${sample}_postAlign.pbs
+echo "echo generating PON vcf took \$(echo a|awk '{print '\"\$ponT\"'/3600}') hours">>jobs/postAlign/${sample}_postAlign.pbs
+echo 'echo PON finished at $(date)'>>jobs/postAlign/${sample}_postAlign.pbs
