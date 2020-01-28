41535706bb20791b8c46aee3e9364ebf1a40bf2e
Jason Dai <jad054@tscc-login3.sdsc.edu>
2020-01-28 15:34:13 -0800

    testing

---
+++ postalign_check_template.sh
@@ -0,0 +1,167 @@
+#!/bin/bash
+
+
+USAGE="USAGE:\trefine_check_template.sh \\
+		map_file \\
+		full_path_to_project_directory\n\n"
+
+map_file=$1
+project_dir=$2
+
+printf "#!/bin/bash
+
+oneGB=1000000
+
+cd ${project_dir}
+
+refine_failed_samples=${project_dir}/jobs/check_and_go/refine_\$(date|awk '{OFS=\"-\";\$1=\$1;print}').error
+varscan_script=${project_dir}/jobs/check_and_go/varscan.sh
+strelka_script=${project_dir}/jobs/check_and_go/strelka.sh
+varscan_job_ID=${project_dir}/jobs/check_and_go/varscan_job_IDs.txt
+strelka_job_ID=${project_dir}/jobs/check_and_go/strelka_job_IDs.txt
+
+printf \"Check refine was performed at \$(date)
+########### ERROR report ##########\\\n\" > \${refine_failed_samples}
+
+printf \"#!/bin/bash
+#Run this after postAlign is done\\\n
+cd ${project_dir}/jobs/varscan/\\\n\\\n\" > \${varscan_script}
+
+printf \"#!/bin/bash
+#Run this after refine is done\\\n
+cd ${project_dir}/jobs/strelka/\\\n\\\n\" > \${strelka_script}
+
+printf \"\" > \${varscan_job_ID}
+printf \"\" > \${strelka_job_ID}
+
+chmod 770 \${refine_failed_samples} \${varscan_script} \${strelka_script} \${varscan_job_ID} \${strelka_job_ID}
+
+
+for sample in \$(tail -n+2 ${map_file} | cut -f1)
+do
+	#stop when there are errors
+	tumor_errors=0
+	normal_errors=0
+
+
+	###################
+	##Check num files##
+	###################
+	
+	bampath=${project_dir}/\$sample
+	target_intervals=\${bampath}/\${sample}_realign_target.intervals
+
+	tbam_mkdup=\${bampath}/\${sample}_tumor_mkdp.bam
+	tbam_bqsr=\${bampath}/\${sample}_tumor_bqsr.grp
+	tbam_final=\${bampath}/\${sample}_tumor_final.bam
+	tbai_final=\${bampath}/\${sample}_tumor_final.bai
+	tbam_indelra=\${bampath}/\${sample}_tumor_idra.bam
+	tbai_indelra=\${bampath}/\${sample}_tumor_idra.bai
+
+	nbam_mkdup=\${bampath}/\${sample}_normal_mkdp.bam
+	nbam_bqsr=\${bampath}/\${sample}_normal_bqsr.grp
+	nbam_final=\${bampath}/\${sample}_normal_final.bam
+	nbai_final=\${bampath}/\${sample}_normal_final.bai
+	nbam_indelra=\${bampath}/\${sample}_normal_idra.bam
+	nbai_indelra=\${bampath}/\${sample}_normal_idra.bai
+
+	pon=\${bampath}/\${sample}_PON.vcf.gz
+	pon_index=\${bampath}/\${sample}_PON.vcf.gz.tbi
+
+
+
+	#check if bam files exist
+	if [ ! -e \${tbam_bqsr} ] || [ ! -e \${tbam_final} ] || [ ! -e \${tbai_final} ] || [ ! -e \${tbam_indelra} ] || [ ! -e \${tbai_indelra} ] || [ ! -e \${target_intervals} ]
+	then
+		printf \"\${sample}_tumor: missing bam files or target intervals\\\n\" >> \${refine_failed_samples}
+		(( tumor_errors ++ ))
+	fi
+
+	if [ ! -e \${nbam_bqsr} ] || [ ! -e \${nbam_final} ] || [ ! -e \${nbai_final} ] || [ ! -e \${nbam_indelra} ] || [ ! -e \${nbai_indelra} ] || [ ! -e \${target_intervals} ]
+	then
+		printf \"\${sample}_normal: missing bam files or target intervals\\\n\" >> \${refine_failed_samples}
+		(( normal_errors ++ ))
+	fi
+
+	#check if PON exists
+	if [ ! -e \${pon} ] || [ ! -e \${pon_index} ]
+	then
+		printf \"\${sample}: missing PON\\\n\" >> \${refine_failed_samples}
+		(( normal_errors ++ ))
+	fi
+
+
+	####################
+	##Check file sizes##
+	####################
+
+	#mkdup too small or either bam <1gb means error
+	if [ \${tumor_errors} -lt 1 ] && [ -e \${tbam_mkdup} ]
+	then
+		tmkdup_size=\"\$(du -b \${tbam_mkdup} | cut -f1)\"
+		tfinal_size=\"\$(du -b \${tbam_final} | cut -f1)\"
+		tidra_size=\"\$(du -b \${tbam_indelra} | cut -f1)\"
+		
+		if [ \${tfinal_size} -lt \$oneGB ] || [ \${tidra_size} -lt \$oneGB ]
+		then
+			printf \"\${sample}_tumor: one or more bam files are too small\\\n\" >> \${refine_failed_samples}
+			(( tumor_errors ++ ))
+		fi
+	fi
+
+	if [ \${normal_errors} -lt 1 ] && [ -e \${nbam_mkdup} ]
+	then
+		nmkdup_size=\"\$(du -b \${nbam_mkdup} | cut -f1)\"
+		nfinal_size=\"\$(du -b \${nbam_final} | cut -f1)\"
+		nidra_size=\"\$(du -b \${nbam_indelra} | cut -f1)\"
+
+		if [ \${nfinal_size} -lt \$oneGB ] || [ \${nidra_size} -lt \$oneGB ]
+		then
+			printf \"\${sample}_normal: one or more bam files are too small\\\n\" >> \${refine_failed_samples}
+			(( normal_errors ++ ))
+		fi
+	fi
+
+	########################
+	##Check file truncated##
+	########################
+
+	#quickcheck returns nothing if the file is ok
+	if [ \${tumor_errors} -lt 1 ]
+	then
+		tfinal_check=\"\$(samtools quickcheck \${tbam_final} | wc -l)\"
+		tidra_check=\"\$(samtools quickcheck \${tbam_indelra} | wc -l)\"
+		
+		if [ \${tfinal_check} -gt 0 ] || [ \${tidra_check} -gt 0 ]
+		then
+			printf \"\${sample}_tumor: Bam file truncated\\\n\" >> \${align_failed_samples}
+			(( tumor_errors ++ ))
+		fi
+	fi
+
+	if [ \${normal_errors} -lt 1 ]
+	then
+		nfinal_check=\"\$(samtools quickcheck \${nbam_final} | wc -l)\"
+		nidra_check=\"\$(samtools quickcheck \${nbam_indelra} | wc -l)\"
+
+		if [ \${nfinal_check} -gt 0 ] || [ \${nidra_check} -gt 0 ]
+		then
+			printf \"\${sample}_normal: Bam file truncated\\\n\" >> \${align_failed_samples}
+			(( normal_errors ++ ))
+		fi
+	fi
+
+	####################
+	##Submit next step##
+	####################
+
+	if [ \${normal_errors} -lt 1 ] && [ \${tumor_errors} -lt 1 ]
+	then
+		echo \"qsub \${sample}_varscan.pbs | awk -F"." '{print \\\$1\\\\\"\\\t\$sample\\\\\"}'>> \${varscan_job_ID}\">>\${varscan_script}
+
+		echo \"qsub \${sample}_strelka.pbs | awk -F"." '{print \\\$1\\\\\"\\\t\$sample\\\\\"}'>> \${strelka_job_ID}\">>\${strelka_script}
+	fi	
+done
+" > ${project_dir}/jobs/check_and_go/refine_check.sh
+
+chmod 770 ${project_dir}/jobs/check_and_go/refine_check.sh
