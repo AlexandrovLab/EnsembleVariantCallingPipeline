c4637ef79a68eb59a68ea3f5cfc36f5ee09a29bf
Jason Dai <jad054@ucsd.edu>
2020-02-28 15:36:43 -0800

    alias quotes

--- setup_env.sh
+++ setup_env.sh
@@ -10,8 +10,8 @@ echo "Successfully installed three environments: evc_main, evc_strelka and evc_g
 
 #Create links
 
-printf "\nalias run_evc=$(pwd)/run_evc.sh\n" >> ~/.bashrc
-printf "alias run_evc_bam=$(pwd)/run_evc_bam.sh\n" >> ~/.bashrc
+printf "\nalias run_evc=\"$(pwd)/run_evc.sh\"\n" >> ~/.bashrc
+printf "alias run_evc_bam=\"$(pwd)/run_evc_bam.sh\"\n" >> ~/.bashrc
 
 #MuSE setup
 $(pwd)/setup_MuSE.sh $1
