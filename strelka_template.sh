e94b1e789b94e2e2c16fb53225e2062f81b97720
Octocoot <heyudou@gmail.com>
2020-02-28 23:35:54 -0800

    Update strelka_template.sh

--- strelka_template.sh
+++ strelka_template.sh
@@ -38,10 +38,14 @@ else
 #PBS -V 
 "
 fi
+
 header="${header}""
 #PBS -N EVC_strelka_${sample}
 #PBS -e ${sample}_strelka.e
 #PBS -o ${sample}_strelka.o
+
+source ~/.bashrc
+source activate evc_strelka
 "
 
 
@@ -51,8 +55,6 @@ strelka_config_genome="configureStrelkaSomaticWorkflow.py --referenceFasta $ref 
 runstrelka="python2 ${out}/${sample}/strelka/runWorkflow.py -m local -j \$(nproc)"
 
 printf "$header">jobs/strelka/${sample}_strelka.pbs
-echo source ~/.bashrc>>jobs/strelka/${sample}_strelka.pbs
-echo source activate evc_strelka>>jobs/strelka/${sample}_strelka.pbs
 echo mkdir -p ${out}/${sample}/strelka>>jobs/strelka/${sample}_strelka.pbs
 if [ "$file_type" == "bam" ]
 then
