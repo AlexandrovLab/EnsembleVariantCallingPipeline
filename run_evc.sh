f41fedd01426e4325be5fc731591570ce4557962
Jason Dai <jad054@ucsd.edu>
2020-02-28 16:28:48 -0800

    run without refine

--- run_evc.sh
+++ run_evc.sh
@@ -11,6 +11,7 @@ known_indel_list=${8}
 base_recalibration_list=${9}
 walltime=${10}
 queue=${11}
+refine=${12}
 
 
 USAGE="\nMissing input arguments..\n
@@ -25,7 +26,8 @@ USAGE:\trun_evc \\
 	known_indel_list \\
 	base_recalibration_list \\
 	max_walltime (hours only) \\
-	queue \n\n"
+	queue \\
+	run refine? (yes or no)\n\n"
 
 	
 if [ -z "${11}" ]
@@ -58,13 +60,18 @@ do
 	normal=$(echo $line|cut -d ' ' -f3)
 	type=$(echo $line|cut -d ' ' -f4)
 	~/EnsembleVaraintCallingPipeline/align_template.sh $email $sample $tumor $normal $ref $path $out $type $walltime $queue
-	~/EnsembleVaraintCallingPipeline/targetInterval_template.sh $email $sample $ref $out ${known_indel_list} $queue
-	~/EnsembleVaraintCallingPipeline/refine_template.sh $email $sample $ref $out ${known_indel_list} ${base_recalibration_list} $walltime $queue
+	
+	if [ "$refine" == "yes" ]
+	then
+		~/EnsembleVaraintCallingPipeline/targetInterval_template.sh $email $sample $ref $out ${known_indel_list} $queue
+		~/EnsembleVaraintCallingPipeline/refine_template.sh $email $sample $ref $out ${known_indel_list} ${base_recalibration_list} $walltime $queue
+	fi
+	
 	~/EnsembleVaraintCallingPipeline/pon_template.sh $email $sample $ref $out $walltime $queue
-	~/EnsembleVaraintCallingPipeline/strelka_template.sh $email $sample $ref $out $type $walltime $queue
-	~/EnsembleVaraintCallingPipeline/varscan_template.sh $email $sample $ref $out $walltime $queue
-	~/EnsembleVaraintCallingPipeline/mutect_template.sh $email $sample $ref $out $pon $type $dbSNP $walltime $queue
-  	~/EnsembleVaraintCallingPipeline/muse_template.sh $email $sample $ref $out $type $dbSNP $walltime $queue
+	~/EnsembleVaraintCallingPipeline/strelka_template.sh $email $sample $ref $out $type $walltime $queue fastq $refine
+	~/EnsembleVaraintCallingPipeline/varscan_template.sh $email $sample $ref $out $walltime $queue $refine
+	~/EnsembleVaraintCallingPipeline/mutect_template.sh $email $sample $ref $out $pon $type $dbSNP $walltime $queue $refine
+  	~/EnsembleVaraintCallingPipeline/muse_template.sh $email $sample $ref $out $type $dbSNP $walltime $queue $refine
 
 
 done
