da9816a28ef82939d99b37797acbbee6b6c38fb4
Octocoot <heyudou@gmail.com>
2020-03-09 13:42:18 -0700

    Update mutect_template.sh

--- mutect_template.sh
+++ mutect_template.sh
@@ -6,6 +6,7 @@ out=$4
 pon=$5
 type=$6
 dbSNP=$7
+intrvalList=$8
 
 # decide --af-of-alleles-not-in-resource based on exome or genome data type
 if [ $type == "exome" ]
@@ -33,18 +34,19 @@ mkdir -p ${out}/${sample}/mutect
 cd ${out}/${sample}/mutect
 "
 
-mutect_cmd="gatk Mutect2 -independent-mates -R $ref -pon $pon --germline-resource $dbSNP --native-pair-hmm-threads \$(nproc) --af-of-alleles-not-in-resource $af --f1r2-tar-gz ${sample}_f1r2.tar.gz --normal-sample ${sample}_normal --input $normal --tumor-sample ${sample}_tumor --input $tumor -O ${out}/${sample}/mutect/${sample}_mutect2_unfiltered.vcf"
+######## Commands ########
+mutect_cmd="gatk Mutect2 -independent-mates -R $ref -pon $pon --germline-resource $dbSNP --native-pair-hmm-threads \$(nproc) --af-of-alleles-not-in-resource $af --f1r2-tar-gz ${sample}_f1r2.tar.gz --normal-sample ${sample}_normal --input $normal --tumor-sample ${sample}_tumor --input $tumor -O ${sample}_mutect2_unfiltered.vcf"
   
 mutect_orientation="gatk LearnReadOrientationModel -I ${sample}_f1r2.tar.gz -O ${sample}_read-orientation-model.tar.gz"
 
-mutect_pileupsum="gatk GetPileupSummaries --java-options \"-Xmx\$(free -h| grep Mem |awk '{split(\$7,a,\"G\");print a[1]-5\"G\"}')\" -I ${out}/${sample}/${sample}_tumor_final.bam -V $dbSNP -L $dbSNP -O ${sample}_getpileupsummaries.table"
+mutect_pileupsum="gatk GetPileupSummaries --java-options \"-Xmx\$(free -h| grep Mem | awk '{split(\$7,a,\"G\");print a[1]-5\"G\"}')\" -I ${out}/${sample}/${sample}_tumor_final.bam -V $dbSNP -L $intrvalList -O ${sample}_getpileupsummaries.table"
 
 mutect_contamin="gatk CalculateContamination -I ${sample}_getpileupsummaries.table -tumor-segmentation ${sample}_segments.table -O ${sample}_calculatecontamination.table"
 
-mutect_filter="gatk FilterMutectCalls -R $ref -V ${out}/${sample}/mutect/${sample}_mutect2_unfiltered.vcf --tumor-segmentation ${out}/${sample}/mutect/${sample}_segments.table --contamination-table ${out}/${sample}/mutect/${sample}_calculatecontamination.table --ob-priors ${out}/${sample}/mutect/${sample}_read-orientation-model.tar.gz -O ${out}/${sample}/mutect/${sample}_mutect2_filtered.vcf"
+mutect_filter="gatk FilterMutectCalls -R $ref -V ${sample}_unfiltered.vcf --tumor-segmentation ${sample}_segments.table --contamination-table ${sample}_contamination.table --ob-priors ${sample}_read-orientation-model.tar.gz -O ${sample}_mutect2_filtered.vcf"
+###########################
 
 printf "$template">jobs/mutect/${sample}_mutect.pbs
-
 echo 'echo === Starting MuTect2 on sample' ${sample} 'at $(date)==='>>jobs/mutect/${sample}_mutect.pbs
 echo 'echo starting mutect command at $(date)....'>>jobs/mutect/${sample}_mutect.pbs
 echo 'mutectS=$SECONDS'>>jobs/mutect/${sample}_mutect.pbs
