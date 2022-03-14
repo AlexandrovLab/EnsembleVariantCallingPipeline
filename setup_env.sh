09cbd4d7303161424867aa7dcc66aedf77b10f76
Octocoot <heyudou@gmail.com>
2022-03-14 14:39:43 -0700

    Update setup_env.sh
    comment out gatk3 environment set up. Replace bash functions with alias

--- setup_env.sh
+++ setup_env.sh
@@ -5,20 +5,23 @@
 echo "Updating/Installing conda environments"
 conda env update -f strelka_env.yml
 conda env update -f main_env.yml
-conda env update -f gatk3_env.yml
+#conda env update -f gatk3_env.yml
 echo "Successfully installed three environments: evc_main, evc_strelka and evc_gatk3"
 
 #Create links
+printf "\n ## EVC setup script shortcuts" >> ./bashrc
+printf "\nalias run_evc=\"$(pwd)/run_evc.sh\"\n" >> ~/.bashrc
+printf "alias run_evc_bam=\"$(pwd)/run_evc_bam.sh\"\n" >> ~/.bashrc
 
-printf "\nfunction run_evc () { $(pwd)/run_evc.sh \$* }\nexport -f run_evc" >> ~/.bashrc
-printf "\nfunction run_evc_bam () { $(pwd)/run_evc_bam.sh \$* }\nexport -f run_evc_bam\n\n" >> ~/.bashrc
 source ~/.bashrc
 
 #MuSE setup
+echo Setting up MuSE....
 $(pwd)/setup_MuSE.sh
 
-echo Registering for gatk3...
-source activate evc_gatk3
-gatk3-register /projects/ps-lalexandrov/shared/GenomeAnalysisTK-3.8-1-0-gf15c1c3ef/GenomeAnalysisTK.jar
-source deactivate
-echo gatk3 registered, environment set up is finished!
+#echo Registering for gatk3...
+#source activate evc_gatk3
+#gatk3-register /projects/ps-lalexandrov/shared/GenomeAnalysisTK-3.8-1-0-gf15c1c3ef/GenomeAnalysisTK.jar
+#source deactivate
+#echo gatk3 registered, environment set up is finished!
+echo Environment setup is finished!
